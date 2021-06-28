163e463babbd8e14b2e3587ce65d3b6b73042c52
givaldogs <givaldowanda@gmail.com>
2021-06-28 20:58:36 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -25,9 +25,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 
 	public User fromDTO(UserDto objDto) {
